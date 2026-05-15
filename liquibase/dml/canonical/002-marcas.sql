c1db86d73e91ad891e3b7c06c1cab0a855e0dfe6
eric-castaneda17 <emcastaneda-2024b@corhuila.edu.co>
2026-05-14 23:07:22 -0500

    Se agrega datos de marca al sistema

---
+++ liquibase/dml/canonical/002-marcas.sql
@@ -0,0 +1,12 @@
+INSERT INTO marca (id, nombre)
+VALUES
+(1, 'Honda'),
+(2, 'Yamaha'),
+(3, 'Suzuki'),
+(4, 'Kawasaki'),
+(5, 'AKT'),
+(6, 'Hero'),
+(7, 'Bajaj'),
+(8, 'Auteco'),
+(9, 'KTM'),
+(10, 'Genérico');
